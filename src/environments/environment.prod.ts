fde32039b84ba763a3d7b9fc691673a5abaea411
piercebring <piercebring@gmail.com>
2021-04-28 19:22:45 -0700

    fix prod stsAuthority environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  stsAuthority: "https://homelyidentityprovider.azurewebsites.net",
+  stsAuthority: "https://homelyidentityprovider.azurewebsites.net/",
   clientRoot: "https://witty-sand-05f46471e.azurestaticapps.net/",
   clientId: "web-client",
   apiRoot: "https://teamhomely.azurewebsites.net/",
