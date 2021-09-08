2debbd8e16ffc44cb25d77f1441a5d6abeaaa545
Liam Zhou <zhouchengzhong77@gmail.com>
2021-09-07 22:20:27 -0600

    Reformat AuthGuard

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,44 +1,55 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { from, Observable } from 'rxjs';
 import { UserClient } from '../shared/restClients/user-client';
-
 import { AuthService } from './auth-service.component';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    private isLoggedIn: boolean;
-    private observer$: Observable<boolean>;
+  private isLoggedIn: boolean;
+  private observer$: Observable<boolean>;
 
-  constructor(private router: Router, private authService: AuthService, private userClient: UserClient) {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private userClient: UserClient
+  ) {
     this.isLoggedIn = !!this.authService.user && !this.authService.user.expired;
     this.authService.loginChanged.subscribe((loggedIn) => {
       this.isLoggedIn = loggedIn;
-
     });
 
     this.authService.isLoggedIn().then((loggedIn) => {
       this.isLoggedIn = loggedIn;
-      
     });
-  } 
+  }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-     return from(this.authService.isLoggedIn().then(isLoggedIn => {
-       this.isLoggedIn = isLoggedIn;
-      if (this.isLoggedIn) { return true; }
-      this.router.navigate(['unauthorized'], { queryParams: { redirect: state.url }, replaceUrl: true });
-      return false;
-    }));
-
-
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return from(
+      this.authService.isLoggedIn().then((isLoggedIn) => {
+        this.isLoggedIn = isLoggedIn;
+        if (this.isLoggedIn) {
+          return true;
+        }
+        this.router.navigate(['unauthorized'], {
+          queryParams: { redirect: state.url },
+          replaceUrl: true,
+        });
+        return false;
+      })
+    );
 
     //console.log(this.isLoggedIn);
     //if (this.isLoggedIn) { return true; }
     //this.router.navigate(['unauthorized'], { queryParams: { redirect: state.url }, replaceUrl: true });
     //return false;
   }
-   
-
-   
-  }
+}
