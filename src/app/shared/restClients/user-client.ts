95bed07195d625e41c46297e1c2cfb80dda68ec4
Liam Zhou <zhouchengzhong77@gmail.com>
2021-09-20 00:14:35 -0600

    Change getUser url according to backend's route change

--- src/app/shared/restClients/user-client.ts
+++ src/app/shared/restClients/user-client.ts
@@ -1,29 +1,33 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from "../../../environments/environment";
+import { environment } from '../../../environments/environment';
 import { ICalendarCreateResponseDto } from '../dtos/calendar-create-response-dto';
 import { ICalendarEventDto } from '../dtos/calendar-event-dto';
 import { IUserDto } from '../dtos/user-dto';
 
 @Injectable({ providedIn: 'root' })
 export class UserClient {
-
   private readonly baseUrl: string;
 
-  constructor(
-    private client: HttpClient
-  ) {
+  constructor(private client: HttpClient) {
     this.baseUrl = environment.apiRoot;
   }
 
   getUserInfo(userName: string): Observable<IUserDto> {
-    let params = new HttpParams();
-    params = params.append("username", userName);
-    return this.client.get<IUserDto>(this.baseUrl + 'user', { params: params });
+    return this.client.get<IUserDto>(`${this.baseUrl}user/${userName}`);
   }
 
-  createEvent(householdId: number, creatorId: number, title: string, description: string, color: string, allDay?: boolean, start?: Date, end?: Date) {
+  createEvent(
+    householdId: number,
+    creatorId: number,
+    title: string,
+    description: string,
+    color: string,
+    allDay?: boolean,
+    start?: Date,
+    end?: Date
+  ) {
     let body = {
       householdId,
       creatorId,
@@ -31,11 +35,14 @@ export class UserClient {
       end,
       title,
       description,
-      color: "",
-      allDay
-    }
-    console.log(body)
-    return this.client.post<ICalendarCreateResponseDto>(this.baseUrl + "Event", body);
+      color: '',
+      allDay,
+    };
+    console.log(body);
+    return this.client.post<ICalendarCreateResponseDto>(
+      this.baseUrl + 'Event',
+      body
+    );
   }
 
   //getApplicantNames(accountId: number): Observable<IApplicantNameDto[]> {
