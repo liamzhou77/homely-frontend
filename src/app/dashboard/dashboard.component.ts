7a818c22f3d5dbea62a20c68bae0cb0c5a210349
Liam Zhou <zhouchengzhong77@gmail.com>
2021-11-29 14:52:38 -0700

    Finish tasks and meals sections in Dashboard page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../core/auth-service.component';
+import { Meal } from '../meals/shared/enums';
+import { MealsService } from '../meals/shared/meals.service';
+import { Task } from '../tasks/shared/task';
+import { TaskService } from '../tasks/shared/tasks.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,23 +11,37 @@ import { AuthService } from '../core/auth-service.component';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  isLoggedIn = false;
+  private householdId: number;
+  public tasks: Task[];
+  public meals: Meal[];
 
-  constructor(private _authService: AuthService) {
-    this._authService.loginChanged.subscribe((loggedIn) => {
-      this.isLoggedIn = loggedIn;
+  constructor(
+    private authService: AuthService,
+    private taskService: TaskService,
+    private mealService: MealsService
+  ) {
+    this.authService.userInfoChanged.subscribe((userInfo) => {
+      this.householdId = userInfo.householdID;
     });
   }
 
-  ngOnInit(): void { }
-
-
-  
-  login() {
-    this._authService.login();
+  ngOnInit(): void {
+    this.getTasks();
+    this.getMeals();
   }
 
-  logout() {
-    this._authService.logout();
+  public getTasks(): void {
+    this.taskService
+      .getTasks(this.householdId, false)
+      .subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  public getMeals(): void {
+    this.mealService.getMeals(this.householdId).subscribe((meals) => {
+      this.meals = meals.filter(
+        (meal) =>
+          new Date(meal.scheduledDate) >= new Date(new Date().toDateString())
+      );
+    });
   }
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatIconModule,
+    RouterModule,
+    MatButtonModule,
+  ],
 })
 export class DashboardModule {}
